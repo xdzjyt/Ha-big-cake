1d96398c0fce5f3ea665d9ee57bd64a1689993f5
Simon <16623819144@163.com>
2024-04-03 21:39:14 +0800

    sass功能测试

--- src/stores/data.ts
+++ src/stores/data.ts
@@ -2,13 +2,16 @@ import { reactive } from "vue";
 import { defineStore } from "pinia";
 
 
-export const datastore = defineStore('data',()=>{
-    const data = reactive({
+export const useThemeStyleStore = defineStore('theme',()=>{
+    const light_style = reactive({
+    });
+    const dark_style = reactive({
         
     });
 
     return {
-        data,
+        light_style,
+        dark_style,
     }
 },{
     persist:false,// 是否选择持续化存储
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,47 @@
 <script setup lang="ts">
+let dark = false;
 
+let modelBtn = () => {
+    dark = !dark;
+    if (dark){
+        window.document.documentElement.setAttribute("data-theme", 'dark');
+    }else {
+        window.document.documentElement.setAttribute("data-theme", 'light');
+    }
+};
 </script>
 
 <template>
-
+    <div id="DarkModePage">
+        <div>
+            <h1 class="title">SImon</h1>
+            <div class="btn" @click="modelBtn">模式切换</div>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "../assets/scss/common.scss";
+#DarkModePage{
+    @include background_color("background_color");
+    @include font_color("font_color");
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background-color 1s,color 0.6s;
+    .title{
+        margin-bottom: 20px;
+    }
+    .btn{
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 40px;
+        margin: 0;
+    }
+}
 </style>
