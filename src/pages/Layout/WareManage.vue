790f337cfc569a3f857536061a71ef83bf6bbf21
Simon应 <13192574+simon-should@user.noreply.gitee.com>
2024-05-22 23:32:03 +0800

    调整物料管理样式

--- src/pages/Layout/WareManage.vue
+++ src/pages/Layout/WareManage.vue
@@ -121,7 +121,7 @@ onMounted(() => {
     <el-scrollbar>
       <el-container>
         <el-main>
-          <el-row :gutter="20">
+          <el-row :gutter="30">
             <el-col :span="14">
               <div class="leftArea">
                 <div class="box1">
@@ -142,57 +142,85 @@ onMounted(() => {
             </el-col>
             <el-col :span="10">
               <div class="rightArea">
+                <div class="search-box">
+                  <el-input v-model="input" placeholder="请输入想要查询的物料" />
+                  <div class="button search" :icon="Search">搜索</div>
+                </div>
                 <div class="box3">
-                  <el-input v-model="input" style="width: 240px; margin: 10px 10px" placeholder="请输入想要查询的物料" />
-                  <el-button type="primary" :icon="Search" style="margin-left: 20px;width: 10%;">搜索</el-button>
-                  <h3 style="margin: 10px 0;">物料快捷操作</h3>
+                  <h3>物料快捷操作</h3>
                   <el-form :model="form" label-width="auto" style="max-width: 600px" :label-position="labelPosition"
                     class="inline-form">
-                    <el-form-item label="物料名称">
-                      <el-input v-model="form[0].name" placeholder="糖浆/牛奶/纸杯" size="large" />
-                    </el-form-item>
-                    <el-image style="width: 150px; height: 150px" :src="imageSrc" :fit="fits" />
-                    <el-image style="width: 150px; height: 150px" :src="imageSrc" :fit="fits" />
-                    <el-form-item label="变化数量">
-                      <el-input v-model="form[1].name" placeholder="增加/减少" size="large" />
-                    </el-form-item>
-                    <el-form-item label="变动类型">
-                      <el-input v-model="form[2].name" placeholder="采购/抽样/损耗" size="large" />
-                    </el-form-item>
-                    <el-form-item label="生产日期">
-                      <el-col :span="24">
-                        <el-date-picker v-model="form[3].name" type="date" placeholder="Pick a date" style="width: 100%"
-                          size="large" />
-                      </el-col>
-                    </el-form-item>
-                    <el-form-item label="保质期/日">
-                      <el-input v-model="form[4].name" style="width: 70%;" placeholder="减少可不填" size="large" />
-                    </el-form-item>
-                    <el-form-item label="物料来源" style="width: 60%;">
+                    <div class="section-1">
+                      <div class="left">
+                        <el-form-item style="width: 100%;">
+                          <label slot="label" style="color:#917800;font-size: 18px;">物料名称</label>
+                          <el-input v-model="form[0].name" placeholder="糖浆/牛奶/纸杯" size="large" />
+                        </el-form-item>
+                        <el-form-item  style="width: 40%;">
+                          <label slot="label" style="color:#917800;font-size: 18px;">变化数量</label>
+                          <el-input v-model="form[1].name" placeholder="增加/减少" size="large" />
+                        </el-form-item>
+                        <el-form-item style="width: 55%;">
+                          <label slot="label" style="color:#917800;font-size: 18px;">变动类型</label>
+                          <el-input v-model="form[2].name" placeholder="采购/抽样/损耗" size="large" />
+                        </el-form-item>
+                      </div>
+                      <div class="right">
+                        <div class="img-box">
+                          <el-image style="width: 100px; height: 100px;border-radius: 12px;" :src="imageSrc" :fit="fits" />
+                          <div class="label">物料实拍图</div>
+                        </div>
+                        <div class="img-box">
+                          <el-image style="width: 100px; height: 100px;border-radius: 12px;" :src="imageSrc" :fit="fits" />
+                          <div class="label">票据实拍图</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="section-2">
+                      <el-form-item  style="width: 28%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">生产日期</label>
+                        <el-date-picker v-model="form[3].name" type="date" placeholder="减少可不填" />
+                      </el-form-item>
+                      <el-form-item  style="width: 28%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">保质期/日</label>
+                        <el-date-picker v-model="form[4].name" placeholder="减少可不填" />
+                      </el-form-item>
+                    </div>
+                    <el-form-item  style="width: 80%;">
+                      <label slot="label" style="color:#917800;font-size: 18px;">物料来源</label>
                       <el-input v-model="form[5].name" placeholder="xx省 xx市 xx区/县 xx公司" size="large" />
                     </el-form-item>
-                    <el-form-item label="单价" style="width: 50%; margin-right: 10px">
-                      <el-input v-model="form[6].name" size="large" />
-                    </el-form-item>
-                    <el-form-item label="总价">
-                      <el-input v-model="form[7].name" size="large" />
-                    </el-form-item>
-                    <el-form-item label="仓位" style="width: 55%; margin-right: 10px;">
-                      <el-input v-model="form[8].name" placeholder="C5-H003-1238" size="large" />
-                    </el-form-item>
-                    <el-form-item label="变动时间">
-                      <el-input v-model="form[9].name" placeholder="2021/9/12" size="large" />
-                    </el-form-item>
-                    <el-form-item label="操作员" style="width: 55%;">
+                    <div class="section-3">
+                      <el-form-item  style="width: 42%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">单价</label>
+                        <el-input v-model="form[6].name" size="large" placeholder="xxxx元(减少可不写)" />
+                      </el-form-item>
+                      <el-form-item   style="width: 42%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">总价</label>
+                        <el-input v-model="form[7].name" size="large" placeholder="xxxx元(减少可不写)" />
+                      </el-form-item>
+                    </div>
+                    <div class="section-4">
+                      <el-form-item  style="width: 50%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">仓位</label>
+                        <el-input v-model="form[8].name" placeholder="C5-H003-1238" />
+                      </el-form-item>
+                      <el-form-item  style="width: 40%;">
+                        <label slot="label" style="color:#917800;font-size: 18px;">变动时间</label>
+                        <el-input v-model="form[9].name" placeholder="2021/9/12" />
+                      </el-form-item>
+                    </div>
+                    <el-form-item style="width: 55%;">
+                      <label slot="label" style="color:#917800;font-size: 18px;">操作员</label>
                       <el-input v-model="form[10].name" placeholder="李华" size="large" />
                     </el-form-item>
-                    <el-form-item label="备注" :rows="6" style=" width: 80%;  margin-right: 100px; margin-bottom: 10px;">
-                      <el-input v-model="form[0].desc" type="textarea" style="height: 100%;" />
+                    <el-form-item  style=" width: 80%;margin-bottom: 20px;">
+                      <label slot="label" style="color:#917800;font-size: 18px;">备注</label>
+                      <el-input v-model="form[0].desc" type="textarea" :rows="6" />
                     </el-form-item>
-                    <el-form-item>
-                      <el-button type="primary">Create</el-button>
-                      <el-button>Cancel</el-button>
-                    </el-form-item>
+                    <div class="button-box">
+                      <div class="button submit">执行操作</div>
+                    </div>
                   </el-form>
                 </div>
               </div>
@@ -231,172 +259,256 @@ onMounted(() => {
   margin: 0;
 }
 
-.el-main {
-  padding: 10px;
-  height: 100vh;
-  box-sizing: border-box;
+.home-section {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  @include background_color('bg-100');
+  transition: all .5s ease-in;
 
-  .leftArea {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  .button {
+    user-select: none;
+    min-width: 100px;
+    padding: 0 20px;
+    height: 40px;
+    outline: none;
+    border: none;
+    @include background_color("bg-300");
+    color: white;
+    font-size: 18px;
+    border-radius: 5px;
+    transition: all 0.3s ease;
+    box-shadow: 0 0 4px rgba(49, 61, 68, 0.5);
+    text-align: center;
+    line-height: 40px;
 
-
-    .box1 {
-      width: 95%;
-      height: 50%;
-      padding: 10px;
-      border-radius: 12px;
-      box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
-      @include background_color('bg-200');
-      transition: all .3s ease;
-
-      &:hover {
-        box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
-        scale: 1.01;
-      }
+    &:hover {
+      scale: 1.03;
+      opacity: 0.8;
     }
 
-    .box2 {
-      width: 95%;
-      height: 50%;
-      padding: 10px;
-      border-radius: 12px;
-      box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
-      @include background_color('bg-200');
-      transition: all .3s ease;
+    &:active {
+      scale: 0.98;
+    }
+  }
 
-      &:hover {
-        box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
-        scale: 1.01;
+  .el-main {
+    padding: 60px 30px 80px 50px;
+
+    .leftArea {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 40px;
+
+
+      .box1 {
+        overflow: hidden;
+        width: 75%;
+        height: 45vh;
+        padding: 20px;
+        border-radius: 12px;
+        box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
+        @include background_color('bg-200');
+        transition: all .3s ease;
+
+        &:hover {
+          box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
+          scale: 1.01;
+        }
       }
 
-      .report-box {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        gap: 20px;
-        @include font_color('text-100');
+      .box2 {
+        width: 95%;
+        height: 60vh;
+        padding: 30px;
+        border-radius: 12px;
+        box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
+        @include background_color('bg-200');
+        transition: all .3s ease;
 
-        h3 {
-          @include font_color('primary-300');
+        &:hover {
+          box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
+          scale: 1.01;
         }
 
-        .report {
-          box-shadow: inset 4px 4px 4px rgba(49, 61, 68, .8);
-          padding: 5px;
-          border-radius: 8px;
-          border: 2px solid;
-          @include border_color('text-200');
-          user-select: none;
+        .report-box {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          gap: 20px;
+          @include font_color('text-100');
 
-          .report-item {
-            padding: 10px 6px;
-            border-bottom: 2px solid;
+          h3 {
+            @include font_color('primary-300');
+          }
+
+          .report {
+            box-shadow: inset 4px 4px 4px rgba(49, 61, 68, .8);
+            padding: 10px;
+            border-radius: 8px;
+            border: 2px solid;
             @include border_color('text-200');
-            transition: all .3s ease;
+            user-select: none;
 
-            &:first-child {
-              @include background_color('bg-300');
-            }
+            .report-item {
+              padding: 10px 6px;
+              border-bottom: 2px solid;
+              @include border_color('text-200');
+              transition: all .3s ease;
 
-            &:last-child {
-              border: none;
-            }
+              &:first-child {
+                @include background_color('bg-300');
+              }
 
-            &:hover {
-              // filter: drop-shadow(0 0 10px #fff) drop-shadow(0 0 20px #fff);
-              box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
-              scale: 1.01;
-            }
+              &:last-child {
+                border: none;
+              }
 
-            &:active {
-              scale: 0.99;
+              &:hover {
+                // filter: drop-shadow(0 0 10px #fff) drop-shadow(0 0 20px #fff);
+                box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
+                scale: 1.01;
+              }
+
+              &:active {
+                scale: 0.99;
+              }
             }
           }
         }
       }
     }
-  }
 
-  .rightArea {
-    height: 100%;
-
-    .box3 {
+    .rightArea {
       height: 100%;
-      width: 100%;
-      padding: 5px;
-      border-radius: 12px;
-      box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
-      @include background_color('bg-200');
-      transition: all .3s ease;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
 
-      &:hover {
-        box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
-        scale: 1.01;
-      }
-
-      h3 {
-        @include font_color('primary-300');
-        padding-left: 30px;
-        display: inline-block;
-        width: 30%;
-      }
-
-      .inline-form {
+      .search-box {
+        width: 100%;
+        height: 15vh;
         display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-        align-items: center;
-        margin-left: 10px;
+        justify-content: space-between;
+        padding: 0 10px;
+
+        .el-input {
+          width: 70%;
+          height: 40%;
+        }
+
+        .search {
+          width: 20%;
+        }
+      }
+
+      .box3 {
+        height: 100%;
+        width: 100%;
+        padding: 30px 20px;
+        border-radius: 12px;
+        box-shadow: 10px 10px 10px rgba(49, 61, 68, .4);
+        @include background_color('bg-200');
+        transition: all .3s ease;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        &:hover {
+          box-shadow: inset 0 0 10px rgba(49, 61, 68, .6);
+          scale: 1.01;
+        }
+
+        h3 {
+          @include font_color('text-100');
+          font-size: 25px;
+          white-space: nowrap;
+          margin-bottom: 20px;
+        }
+
+        .inline-form {
+
+          .el-input {
+            height: 35px;
+            margin-bottom: 10px;
+          }
+
+          .section-1 {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            gap: 20px;
+
+            .left {
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: space-between;
+              align-items: center;
+            }
+
+            .right {
+              display: flex;
+              justify-content: space-between;
+              gap: 10px;
+              align-items: center;
+              .label{
+                @include font_color('text-100');
+                margin-top: 4px;
+                font-weight: 550;
+              }
+            }
+          }
+
+          .section-2,
+          .section-3,
+          .section-4 {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 20px;
+            margin-bottom: 10px;
+          }
+
+          .button-box {
+            display: flex;
+            justify-content: flex-end;
+            .submit {
+              @include background_color('bg-300');
+
+            }
+          }
+
+
+        }
       }
     }
   }
-}
 
-.el-footer {
-  padding: 0 !important;
+  .el-footer {
+    padding: 0 !important;
 
-  .copyright {
-    height: 80px;
-    width: 100%;
-    @include background_color('bg-300');
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    box-shadow: 0 -5px 10px rgba(49, 61, 68, .6);
+    .copyright {
+      height: 80px;
+      width: 100%;
+      @include background_color('bg-300');
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      box-shadow: 0 -5px 10px rgba(49, 61, 68, .6);
+    }
+
+    .more {
+      padding-top: 60px;
+      @include font_color('text-200');
+      font-size: 25px;
+      font-weight: 500;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
-
-  .more {
-    padding-top: 60px;
-    @include font_color('text-200');
-    font-size: 25px;
-    font-weight: 500;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 }
-
-// .el-row {
-//   margin-bottom: 20px;
-//   height: 100%;
-// }
-
-// .el-row:last-child {
-//   margin-bottom: 0;
-// }
-
-// .el-col {
-//   border-radius: 4px;
-//   height: 100%;
-// }
-
-// .grid-content {
-//   border-radius: 4px;
-//   min-height: 36px;
-// }
 </style>
